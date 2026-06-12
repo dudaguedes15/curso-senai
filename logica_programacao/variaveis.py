732d7bda14a49e232f5cf81b3c31c3ddece724c6
duda guedes <duda.mra.farias@gmail.com>
2026-06-12 14:58:24 -0300

    12/06 curso

---
+++ logica_programacao/variaveis.py
@@ -0,0 +1,79 @@
+# variáveis -- armazenar valor, e esse valor pode variar.
+
+nome = "Giovani" 
+idade = 24
+altura = 1.35
+tem_mundial = False 
+
+print(nome)
+print(idade)
+print(altura)
+print(tem_mundial)
+
+nome = "Duda"
+fruta = "Banana"
+time = "Palmeiras"
+cidade = "Paranavaí"
+animal = "Macaco"
+
+print(nome)
+print(fruta)
+print(time)
+print(cidade)
+print(animal)
+
+idade = 16
+quantidade = 6
+jogadores = 12
+habitantes = 30000000
+patas = 4 
+
+print(idade)
+print(quantidade)
+print(jogadores)
+print(habitantes)
+print(patas)
+
+altura = 1.60
+kg = 5.6
+km = 9.0
+dinheiro = 50.00
+litros = 3.5
+
+print(altura)
+print(kg)
+print(km)
+print(dinheiro)
+print(litros)
+
+leu_livros = True
+gosta_de_cafe = True
+engraçado = False 
+farma_aura = False
+ego = False
+
+print(leu_livros)
+print(gosta_de_cafe)
+print(engraçado)
+print(farma_aura)
+print(ego)
+
+nome = input("Qual o seu nome?: ")
+fruta = input("Fale uma fruta: ")
+print(nome)
+print(fruta)
+
+idade = int(input("Fale a sua idade: "))
+quantidade_frutas = int(input("Quantas frutas você comprou: "))
+print(idade)
+print(quantidade_frutas)
+
+altura = float(input("Fale a sua altura: "))
+dinheiro = float(input("Fale quanto de dinheiro você tem?"))
+print(altura)
+print(dinheiro)
+
+leu_livros = input("Você leu livros? S/N|: ")
+engraçado = input("S/N: ")
+print(leu_livros)
+print(engraçado)
